f5223cee99af7ce8d3f2a9e6c6f8f46f4edbeac4
m-evdokimov <m.v.evdokimov1@gmail.com>
2020-11-10 10:24:02 +0500

    Add main.py and write ahp

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def ahp():
+    n = None
+
+    while 1:
+        try:
+            print('Enter a number of categories: ')
+            n = int(input())
+        except ValueError:  # finding non-int values
+            print('Incorrect input! Try again')
+            continue
+        break
+
+    M = np.ones((n, n))
+
+    for i in range(M.shape[0]):
+        for j in range(M.shape[1]):
+            if i < j:
+                while 1:
+                    try:
+                        wij = float(input(f'\nHow many times weight_{i}  more important than weight_{j}?\n'))
+                    except ValueError:  # finding non-float values
+                        print('Incorrect input! Try again')
+                        continue
+                    break
+
+                M[i, j] = wij
+                M[j, i] = 1 / wij
+
+    rowsum_M = M.sum(axis=1)
+    sum = M.sum()
+    weights = rowsum_M / sum
+    weights = np.around(weights, decimals=2)
+    print('AHP weights: ', weights)
+
+
+
+def main():
+    ahp()
+
+if __name__ == '__main__':
+    main()
